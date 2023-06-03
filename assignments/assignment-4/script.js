870c253ed80dfd0ac0b56ffe07c1f740e89998e9
Tohir Kuliev <kuliev.tech@gmail.com>
2023-06-03 17:37:33 -0400

    finish assignment-4 partly

---
+++ assignments/assignment-4/script.js
@@ -0,0 +1,21 @@
+var part1 = "The greatest glory in";
+var part2 = "living lies not in";
+var part3 = "never falling, but in";
+var part4 = "rising every time";
+var part5 = "we fall";
+var author = "Nelson Mandela";
+
+// template literals method
+
+var result = `${part1} ${part2} ${part3} ${part4} ${part5}`;
+console.log(result);
+
+// contact() method
+
+var result = console.log(result);
+
+// + operator method
+
+var result = console.log(result);
+
+// += operator method
