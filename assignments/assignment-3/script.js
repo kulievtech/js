80b5e274a2eff90ecf2d56c5156b8afe4dfac229
Tohir Kuliev <kuliev.tech@gmail.com>
2023-06-03 17:20:27 -0400

    finish assignment-3 partly

---
+++ assignments/assignment-3/script.js
@@ -0,0 +1,35 @@
+// Part 1
+
+var num1 = 5;
+var num2 = 10;
+
+var sum = num1 + num2;
+console.log(sum);
+
+var difference = num1 - num2;
+console.log(difference);
+
+var multiply = num1 * num2;
+console.log(multiply);
+
+let divide = num1 / num2;
+console.log(divide);
+
+// Part 2
+
+var fahrenheit = 100;
+var celsius = (fahrenheit - 32) * (5 / 9);
+console.log(celsius);
+
+var celsius = 38;
+var fahrenheit = celsius * (9 / 5) + 32;
+console.log(fahrenheit);
+
+// Part 3
+
+var principal = 500;
+var rate = 0.095;
+var time = 6;
+
+var interest = (principal * rate * time) / 100;
+console.log(interest);
