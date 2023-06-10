2b574b32e7a600c460881e48efc2de11893693de
Tohir Kuliev <kuliev.tech@gmail.com>
2023-06-10 15:40:30 -0400

    finish in class assignment

--- lectures/nodejs-env/script.js
+++ lectures/nodejs-env/script.js
@@ -1,1 +1,11 @@
-console.log("Tohir");
+// create a function that takes two strings, and returns a string that have more characters. "hello", "hi"
+
+function longerString(str1, str2) {
+    if (str1.length > str2.length) {
+        return str1;
+    } else {
+        return str2;
+    }
+}
+
+console.log(longerString("Hello", "hi"));
