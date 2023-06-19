7ff7eadb05a01bf5af944d9aa68e8d9e02a2c82d
Tohir Kuliev <kuliev.tech@gmail.com>
2023-06-19 10:54:40 -0400

    finish assignment-18

--- assignments/assignment-18/script.js
+++ assignments/assignment-18/script.js
@@ -123,3 +123,173 @@ console.log(binaryCounter([0, 1, 0, 1, 1, 0, 0, 1])); // output: [4,4]
 //Example: uniqueStringFilter(["Hello", "hello", "World", "world", "WORLD", "hello", "HELLO", "Test"]);
 
 //This should return ["Hello", "World", "Test"], because all other strings are duplicates (ignoring case) of these.
+
+function uniqueStringFilter(array) {
+    var newArray = [];
+    var lowercaseArray = [];
+
+    for (var i = 0; i < array.length; i++) {
+        var str = array[i];
+        var lowercaseString = str.toLowerCase();
+
+        if (!lowercaseArray.includes(lowercaseString)) {
+            lowercaseArray.push(lowercaseString);
+            newArray.push(str);
+        }
+    }
+
+    return newArray;
+}
+
+console.log(
+    uniqueStringFilter([
+        "Tom",
+        "tom",
+        "Jerry",
+        "jerry",
+        "JERRY",
+        "tom",
+        "TOM",
+        "CAT"
+    ])
+); // output: [ 'Tom', 'Jerry', 'CAT' ]
+console.log(
+    uniqueStringFilter([
+        "Hello",
+        "hello",
+        "World",
+        "world",
+        "WORLD",
+        "hello",
+        "HELLO",
+        "Test"
+    ])
+); // output: [ 'Hello', 'World', 'Test' ]
+
+/// part 6
+
+//Odd Number Summation
+
+//Create a function named oddNumberSum that accepts an array of numbers as an argument. The function should return the sum of all odd numbers in the array.
+
+//Example: oddNumberSum([1, 2, 3, 4, 5, 6, 7, 8, 9]); should return 25 because the sum of the odd numbers is 1 + 3 + 5 + 7 + 9 = 25.
+
+//Edge Case: oddNumberSum([]); should return 0 because there are no numbers to sum.
+
+function oddNumberSum(array) {
+    var totalSum = 0;
+
+    for (var i = 0; i < array.length; i++) {
+        var num = array[i];
+
+        if (num % 2 !== 0) {
+            totalSum = totalSum + num;
+        }
+    }
+    return totalSum;
+}
+
+console.log(oddNumberSum([1, 2, 2, 4, 4, 7, 2, 8, 9])); // output: 17
+
+// part 7
+
+//Reverse Array
+
+//Create a function named reverseString that accepts a an array of english letters as an argument. Return a new reversed array. Do not use reverse() or toReversed() methods.
+
+//Example: reverseString(["H", "e", "l", "l", "o"]); should return [ 'o', 'l', 'l', 'e', 'H' ]
+
+//Edge Case:
+
+//reverseString([]); should return [] because the input array is empty.
+
+//reverseString(["a"]); should return ["a"] because the input array has only one element.
+
+function reverseString(array) {
+    var newArray = [];
+    for (var i = array.length - 1; i >= 0; i--) {
+        var str = array[i];
+        newArray.push(str);
+    }
+    return newArray;
+}
+
+console.log(reverseString(["H", "e", "l", "l", "o"])); // output: [ 'o', 'l', 'l', 'e', 'H' ]
+
+// part 8
+
+//Factorial Calculator
+
+//Create a function named calculateFactorial that accepts a number as an argument. The function should return the factorial of the number. The factorial of a non-negative integer n is the product of all positive integers less than or equal to n.
+
+//Example: calculateFactorial(5); should return 120, because 5*4*3*2*1 = 120.
+
+//Edge Case: calculateFactorial(0); should return 1 because the factorial of 0 is 1 by definition.
+
+function calculateFactorial(number) {
+    if (number === 0) {
+        return 1;
+    }
+
+    var factorial = 1;
+
+    for (var i = 1; i <= number; i++) {
+        factorial = factorial * i;
+    }
+    return factorial;
+}
+
+console.log(calculateFactorial(5)); // output: 120
+
+// part 9
+
+//Array Element Multiplier
+
+//Task: Create a function named arrayElementMultiplier that accepts an array of numbers. The function should return a new array where each element is twice the value of the corresponding element in the original array.
+
+//Example: arrayElementMultiplier([1, 2, 3, 4, 5]); should return [2, 4, 6, 8, 10].
+
+//Edge Case: arrayElementMultiplier([]); should return [] because the input array is empty.
+
+function arrayElementMultiplier(array) {
+    var newArray = [];
+
+    for (var i = 0; i < array.length; i++) {
+        var num = array[i];
+
+        var double = num * 2;
+        newArray.push(double);
+    }
+    return newArray;
+}
+
+console.log(arrayElementMultiplier([1, 2, 3, 4, 5])); // output: [ 2, 4, 6, 8, 10 ]
+
+// part 10
+
+//Even Index Summation
+
+//Create a function named evenIndexSum that accepts an array of numbers as an argument. The function should return the sum of numbers which are located at even indices in the array.
+
+//Example 1: evenIndexSum([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]); should return 30, because the sum of numbers at even indices (0, 2, 4, 6, 8) is 1 + 3 + 5 + 7 + 9 = 25.
+
+//Example 2: evenIndexSum([10, 20, 30, 40, 50]); should return 90, because the sum of numbers at even indices (0, 2, 4) is 10 + 30 + 50 = 90.
+
+//Edge Case 1: evenIndexSum([]); should return 0, because the array is empty, hence no elements to sum.
+
+//Edge Case 2: evenIndexSum([100]); should return 100, because the array only has one element, which is located at index 0 (an even index).
+
+function evenIndexSum(array) {
+    var totalSum = 0;
+
+    for (var i = 0; i < array.length; i++) {
+        var num = array[i];
+
+        if ([i] % 2 === 0) {
+            totalSum = totalSum + num;
+        }
+    }
+    return totalSum;
+}
+
+console.log(evenIndexSum([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])); // output: 25
