49a76340c4860e220073254398fd65338398bc92
Tohir Kuliev <kuliev.tech@gmail.com>
2023-07-17 21:25:29 -0400

    finish assignment-28

---
+++ assignments/assignment-28/script.js
@@ -0,0 +1,50 @@
+let users = [
+    { id: 1, name: "Smith" },
+    { id: 2, name: "Alex" },
+    { id: 3, name: "John" },
+    { id: 4, name: "Victor" },
+    { id: 5, name: "Alice" },
+    { id: 6, name: "Louis" },
+    { id: 7, name: "Samantha" }
+];
+
+let library = [
+    {
+        bookName: "abcd",
+        genres: ["humor", "non-fiction", "romance"],
+        ratings: [
+            { userId: 1, score: 7 },
+            { userId: 2, score: 9 },
+            { userId: 3, score: 4 }
+        ]
+    },
+    {
+        bookName: "efg",
+        genres: ["biography", "fiction"],
+        ratings: [
+            { userId: 1, score: 5 },
+            { userId: 2, score: 3 },
+            { userId: 3, score: 9 },
+            { userId: 4, score: 2 }
+        ]
+    },
+    {
+        bookName: "hij",
+        genres: ["cookbook", "narrative", "humor"],
+        ratings: [
+            { userId: 1, score: 8 },
+            { userId: 2, score: 5 }
+        ]
+    },
+    {
+        bookName: "klm",
+        genres: ["mystery", "literary fiction", "thriller", "drama", "fantasy"],
+        ratings: [
+            { userId: 1, score: 7 },
+            { userId: 2, score: 9 },
+            { userId: 5, score: 1 },
+            { userId: 6, score: 4 },
+            { userId: 7, score: 5 }
+        ]
+    }
+];
